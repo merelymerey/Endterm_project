d40aec70b9a2c69dca539eb412cb8f2eee402236
merey <merely8merey@gmail.com>
2026-03-16 13:35:43 +0500

    Add accounts URL routing

---
+++ accounts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register_view, name='register'),
+]
