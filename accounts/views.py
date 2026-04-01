b72f79dc6e19382b0e9bcbd0391cf9926ce86d93
merey <merely8merey@gmail.com>
2026-04-01 12:35:32 +0500

    Add forgot_password view with email sending

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.models import User
-from .forms import RegisterForm, LoginForm
+from django.core.mail import send_mail
+from django.conf import settings
+from .forms import RegisterForm, LoginForm, ForgotPasswordForm, VerifyResetCodeForm, ResetPasswordForm
+from .models import PasswordResetCode
 
 
 class CustomLoginView(LoginView):
@@ -18,7 +21,6 @@ class CustomLogoutView(LogoutView):
 def register_view(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
-
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
@@ -32,3 +34,25 @@ def register_view(request):
     else:
         form = RegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
+
+
+def forgot_password_view(request):
+    if request.method == 'POST':
+        form = ForgotPasswordForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            user = User.objects.filter(email=email).first()
+            if user:
+                code = PasswordResetCode.generate_code()
+                PasswordResetCode.objects.create(email=email, code=code)
+                send_mail(
+                    subject='Password Reset Code',
+                    message=f'Your password reset code is: {code}',
+                    from_email=settings.DEFAULT_FROM_EMAIL,
+                    recipient_list=[email],
+                )
+            request.session['reset_email'] = email
+            return redirect('verify_reset_code')
+    else:
+        form = ForgotPasswordForm()
+    return render(request, 'accounts/forgot_password.html', {'form': form})
