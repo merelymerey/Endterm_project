71b0a545a7443c8419fea35a521d8315ba0a1c8a
merey <merely8merey@gmail.com>
2026-04-03 19:19:45 +0500

    Add password reset URLs to accounts routing

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('register/', views.register_view, name='register'),
+    path('forgot-password/', views.forgot_password_view, name='forgot_password'),
+    path('verify-reset-code/', views.verify_reset_code_view, name='verify_reset_code'),
+    path('resend-reset-code/', views.resend_reset_code_view, name='resend_reset_code'),
+    path('reset-password/', views.reset_password_view, name='reset_password'),
 ]
